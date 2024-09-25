6aa1926185ccd5b114f8e325f703a6e55a8fe09a
Shashwat Wazalwar <98304037+VazelWAR@users.noreply.github.com>
2024-09-25 15:08:06 +0530

    Delete Pet-Backend-main directory

--- Pet-Backend-main/Routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login, logout } = require("../Controller/AuthController");
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
-
-module.exports = router;
